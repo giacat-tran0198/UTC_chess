647c777912a6d4670edbbd8f3c603c287c758d8c
Linh Nguyen <tran-khanh-linh.nguyen@etu.utc.fr>
2018-11-24 23:35:21 +0100

    Config project
    [] unloaded static file "chesspieces"
    [] link database
    [] create page "profil"

--- profil/urls.py
+++ profil/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-app_name = 'game'
+
+app_name = 'profil'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- webChess/settings.py
+++ webChess/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
+    'profil',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +125,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
