8abdd3a4be63b3e3256f06d2d99284ff877634a6
Linh Nguyen <khanhlinhnt@outlook.com>
2019-01-04 09:51:48 +0100

    Modified profil

--- profil/views.py
+++ profil/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from profil.models import Player
 from django.contrib.auth.models import User
+import math
 
 
 # Create your views here.
 @login_required
 def index(request):
 	player = Player.objects.get(user__username = request.user)
-	rate_of_win = player.no_of_wins / player.no_of_games
+	rate_of_win = round(player.no_of_wins / player.no_of_games,2)*100
 	return render(request, "profil.html", {"player":player, "rate": rate_of_win})
